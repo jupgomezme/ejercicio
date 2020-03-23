42062b1ac6ddaa2a1564596f7062d25b2f1574c0
jupgomezme <juangomezcol.10@gmail.com>
2020-03-22 21:50:49 -0500

    hola mundo

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -36,7 +36,7 @@ public class Controller {
 			int option = lector.nextInt();
 			switch(option){
 				case 1:
-					System.out.println("--------- \nCrear Arreglo \nDar capacidad inicial del arreglo: ");
+					System.out.println("--------- \nCrear otra cosa. \nDar capacidad inicial del arreglo: ");
 				    int capacidad = lector.nextInt();
 				    modelo = new MVCModelo(capacidad); 
 					System.out.println("Arreglo Dinamico creado");
